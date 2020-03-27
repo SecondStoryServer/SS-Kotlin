671a72cb97a2ed6ab11c497cd49858ea05b530bc
sya-ri <sya79lua@gmail.com>
2020-03-27 17:23:13 +0900

    ss-kotlin コマンドを追加し、 一覧のフォーマットを変更しました

--- src/main/kotlin/me/syari/ss/kotlin/Main.kt
+++ src/main/kotlin/me/syari/ss/kotlin/Main.kt
@@ -1,14 +1,28 @@
 package me.syari.ss.kotlin
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main: JavaPlugin() {
+    private val kotlinDescription = """
+            §6--------[ §a§lSS-Kotlin §6]--------
+            §6 * §dkotlin-jvm §7- §dversion 1.3.71
+            §6 * §dkotlin-stdlib-jdk8
+            §6 * §dkotlin-reflect
+            §6-----------------------------
+        """.trimIndent().lines().toTypedArray()
+
     override fun onEnable() {
-        logger.info("""
-            -----[ SS-Kotlin ]-----
-            * kotlin-stdlib-jdk8
-            * kotlin-reflect
-            -----------------------
-        """.trimIndent())
+        server.consoleSender.sendMessage(kotlinDescription)
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        return if (sender.isOp) {
+            sender.sendMessage(kotlinDescription)
+            true
+        } else {
+            false
+        }
     }
 }
